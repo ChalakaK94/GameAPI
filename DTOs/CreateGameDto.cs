40f6f715acfcf899d16b231c36ce6d935f780dd1
Chalaka Karunarathne <chalaka.karunarathne@kochasoft.com>
2024-05-10 17:49:31 +0530

    Base crud opperation with happy path

---
+++ DTOs/CreateGameDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStore.API.DTOs;
+
+public record class CreateGameDto
+(
+string Name,
+string Genre,
+decimal Price,
+DateOnly ReleaseDate
+);
---
+++ DTOs/UpdateGameDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStore.API.DTOs;
+
+public record class UpdateGameDto(
+
+string Name,
+string Genre,
+decimal Price,
+DateOnly ReleaseDate
+);
