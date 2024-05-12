baa45dd2b618a37ca79d820e1180b259e87ed68a
Chalaka Karunarathne <chalaka.karunarathne@kochasoft.com>
2024-05-12 14:55:07 +0530

    add entity famework , initiate migration

---
+++ Data/DataExtenctions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.API.Data;
+
+public static class DataExtenctions
+{
+
+    public static void MigrateDb(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        dbContext.Database.Migrate();
+    }
+}
--- obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47a4088c1c05c1a069b32bac04a67e8857a67c2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b09eb2f3a34dc14d63170b4b1b37e65acdae3748")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
