6fe41f8c418cb8961f43fd2d82258a5518d439a7
Chalaka Karunarathne <chalaka.karunarathne@kochasoft.com>
2024-05-12 16:12:32 +0530

    seed data migration

--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -11,4 +11,15 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options)
 
     public DbSet<Genre> Genres => Set<Genre>();
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Fighting" },
+            new { Id = 2, Name = "Sports" },
+            new { Id = 3, Name = "Racing" },
+            new { Id = 4, Name = "Adventure" },
+            new { Id = 5, Name = "Kids and Family" }
+        );
+    }
+
 }
--- obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b09eb2f3a34dc14d63170b4b1b37e65acdae3748")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58c2df846bd3da2362a86828fd2e83ceff3b285")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
